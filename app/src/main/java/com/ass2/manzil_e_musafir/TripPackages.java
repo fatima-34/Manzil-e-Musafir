ee2b7af4549d1671b4c8eada4f2300c3240eb1c5
fatima-34 <fatimafarooq143072@gmail.com>
2023-11-09 17:36:23 +0500

    Initial commit

---
+++ app/src/main/java/com/ass2/manzil_e_musafir/TripPackages.java
@@ -0,0 +1,14 @@
+package com.ass2.manzil_e_musafir;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TripPackages extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_packages);
+    }
+}
